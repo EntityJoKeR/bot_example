7dea945f15dee3efb2d27e57d167fb85145fbad7
Futik1 <dg2004_din@mail.ru>
2023-09-20 14:04:23 +0600

    ...

--- admin.py
+++ admin.py
@@ -24,7 +24,7 @@ async def add_employee(message: types.Message):
             await message.reply('Данная команда должна быть отправлена на сообщение сотрудника которого вы хотите добавить в бота')
     else:
         await message.reply('Эта команда доступна только владельцу бота')
-        
+    await message.delete()
         
 @dp.message_handler(lambda message: 'Удалить сотрудника' in message.text)
 async def delete_admin(message: types.message):
@@ -36,7 +36,7 @@ async def delete_admin(message: types.message):
             await message.reply('Данная команда должна быть отправлена на сообщение сотрудника которого вы хотите удалить из бота')
     else:
         await message.reply('Эта команда доступна только владельцу бота')
-    
+    await message.delete()
     
 @dp.message_handler(lambda message: 'Добавить товар' in message.text, state=None)
 async def start_upload(message: types.Message):
@@ -50,6 +50,33 @@ async def start_upload(message: types.Message):
         await message.reply('Загрузите фото')
     else:
         await message.answer('Данная команда вам недоступна')
+    await message.delete()
+
+@dp.message_handler(lambda message: 'Удалить товар' in message.text)
+async def delete_product(message: types.Message):
+    admins = list(db.admins.find())
+    admins_id = []
+    product_name = str(message.text).split()
+    for item in admins:
+        admins_id.append(item['admin_id'])
+    if int(message.from_user.id) == owner_id or int(message.from_user.id) in admins_id:
+        if len(product_name) == 3:
+            print(product_name)
+            product = db.market.find_one({'name': product_name[2]})
+            if product is not None:
+                db.market.delete_one({'name': product_name[2]})
+                await message.answer(f'Товар {product_name[2]} удален.')
+            else:
+                await message.answer(f'Товар {product_name[2]} не найден, возможно вы не указали название товара или указали его неправильно, попробуйте еще раз(название должно быть указано с учетом  регистра).')
+        else:
+            await message.answer(
+                f'Товар не найден, вы не указали название товара, попробуйте еще раз c указанием названия товара.')
+    else:
+        await message.answer('У вас недостаточно прав для использования этой команды')
+
+    await message.delete()
+
+
     
 @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
 async def add_photo(message: types.Message, state: FSMContext):
